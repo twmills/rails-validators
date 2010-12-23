fa7ce474f3e705afdcf8fdadf6a68949a015bc92
Kristian Mandrup <kmandrup@gmail.com>
2010-12-24 00:56:24 +0100

    fixed minor bug

--- lib/rails-validators/company_name_validator.rb
+++ lib/rails-validators/company_name_validator.rb
@@ -2,7 +2,7 @@ class CompanyNameValidator < ActiveModel::EachValidator
   include NameValidator
 
   def validate_each(record, attribute, value)
-    record.errors[attribute] << "is too long" if value.size > max_length    
+    record.errors[attribute] << "is too long" if value && value.size > max_length    
     record.errors[attribute] << "has invalid characters" unless value =~ /^[a-zA-Z0-9\_\-\ ]*?$/
   end
 end
